1e1c18cf74e99a9eb01e85fe3148ad605a32c699
Danielle Stanton <delisestanton@gmail.com>
2019-05-02 18:34:56 -0400

    added to components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,8 +8,9 @@ class StorePicker extends React.Component {
     // 1. stop the form from submitting
     event.preventDefault();
     // 2. get the text from that input
-    console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
     // 3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   };
 
   render() {
